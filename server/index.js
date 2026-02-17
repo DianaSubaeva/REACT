d914c8007c4b3e99e79096de7ce21aca4152426d
Diana <dianasubaeva439gmail.com>
2026-02-17 22:10:13 +0300

    лабораторная 2

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
+import path from 'path';
 import sequelize from './config/database.js';
 import cors from 'cors'; 
 import router from './routes/index.js';
+import errorMiddleware from './middleware/ErrorHandlingMiddleware.js';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 5000;
 
@@ -13,6 +19,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/', router);
+app.use(errorMiddleware);
+app.use('/static', express.static(path.resolve(__dirname, 'static')));
 
 const start = async () => {
     try {
